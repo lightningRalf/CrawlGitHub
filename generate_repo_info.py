b3bd162eed46a4ee1f117afef7c2f012f7ba3e77
Ralf <lightningRalf@skiff.com>
2023-04-14 13:00:32 +0200

    added the readme_url function

--- generate_repo_info.py
+++ generate_repo_info.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import os
+import re
 
 def get_readme_url(github_url, default_branch):
     readme_url = github_url.replace("github.com", "raw.githubusercontent.com") + f"/{default_branch}/README.md"
@@ -59,11 +60,14 @@ for i, github_url in enumerate(urls, start=1):
             markdown_file = f"78.{str(i).zfill(2)}_{repo_name}.md"
 
         # Open the markdown file in append mode
-        with open(markdown_file, "a") as f:
+        with open(markdown_file, "a", encoding="utf-8") as f:
             # Check if the file is empty
             if os.stat(markdown_file).st_size == 0:
                 # Write the repo description and URL
                 f.write(f"## [{repo_description}]({github_url})\n")
+                
+                # Add the iframe with the README.md
+                readme_url = get_readme_url(github_url, default_branch)
                 f.write(f"\n## README\n")
                 f.write(f'<iframe width="100%" height="800" src="{readme_url}" ></iframe>\n')
                 

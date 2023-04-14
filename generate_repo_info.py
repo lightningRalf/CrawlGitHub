59c7c740b5edd7c8cb196d2bc4da916641c1b3fb
Ralf <lightningRalf@skiff.com>
2023-04-14 13:04:59 +0200

    deleted brackets

--- generate_repo_info.py
+++ generate_repo_info.py
@@ -19,7 +19,7 @@ with open("78.01_CrawlGitHubURLs.md", "r") as f:
 for i, github_url in enumerate(urls, start=2):
     # Get the API URL for the GitHub repository
     api_url = github_url.replace("https://github.com", "https://api.github.com/repos")
-    headers = {"Authorization": f"token {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}"}
+    headers = {"Authorization": f"token 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"}
     response = requests.get(api_url, headers=headers)
     time.sleep(1)
     

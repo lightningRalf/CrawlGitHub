5133be3bea3e509874a4a7bbed31d950f891ba43
Ralf <lightningRalf@skiff.com>
2023-04-14 12:54:58 +0200

    removed webcrawling and just relied on the API

--- generate_repo_info.py
+++ generate_repo_info.py
@@ -1,70 +1,55 @@
 import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
 import os
-import json
-import re
+
+def get_readme_url(github_url, default_branch):
+    readme_url = github_url.replace("github.com", "raw.githubusercontent.com") + f"/{default_branch}/README.md"
+    return readme_url
 
 # Set the working directory
 working_directory = "C:\\Users\\mjpa\\Documents\\Obsidian\\70-79 Quellen\\78_GitHub"
 os.chdir(working_directory)
-
-def get_readme_url(github_url):
-    # Get the default branch name
-    api_url = github_url.replace("https://github.com", "https://api.github.com/repos")
-    api_response = requests.get(api_url)
-    if api_response.status_code == 200:
-        default_branch = api_response.json().get("default_branch", "main")
-    else:
-        default_branch = "main"
-    
-    # Generate the URL for the README file
-    readme_url = github_url.replace("github.com", "raw.githubusercontent.com") + f"/{default_branch}/README.md"
-    return readme_url
 
 # Read the URLs from the file
 with open("78.01_CrawlGitHubURLs.md", "r") as f:
     urls = [url.strip() for url in f.readlines() if url.strip()]
 
 # Process each URL
-for i, github_url in enumerate(urls, start=2):
-    # Send an HTTP request to the GitHub URL
-    response = requests.get(github_url)
+for i, github_url in enumerate(urls, start=1):
+    # Get the API URL for the GitHub repository
+    api_url = github_url.replace("https://github.com", "https://api.github.com/repos")
 
-    # Check if the request was successful
+    # Send an HTTP request to the GitHub API URL
+    response = requests.get(api_url)
+
     if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.text, 'html.parser')
+        repo_data = response.json()
 
         # Extract the required information
-        stars = soup.select_one('.Link--muted[href$="stargazers"] strong').text.strip()
-        watchers = soup.select_one('.Link--muted[href$="watchers"] strong').text.strip()
-        forks = soup.select_one('.Link--muted[href$="forks"] strong').text.strip()
-        repo_description = soup.select_one('.public p[itemprop="description"]').text.strip()
+        repo_name = repo_data["name"]
+        repo_description = repo_data["description"] or "No description found"
+        stars = repo_data["stargazers_count"]
+        watching = repo_data["subscribers_count"]
+        forks = repo_data["forks_count"]
+        default_branch = repo_data["default_branch"]
+        contributors_url = repo_data["contributors_url"]
+        releases_url = repo_data["releases_url"].replace("{/id}", "")
 
         # Get the contributors count
-        api_url = f"https://api.github.com/repos/{github_url.split('/')[-2]}/{github_url.split('/')[-1]}/contributors"
-        headers = {"Accept": "application/vnd.github+json"}
-        contributors_response = requests.get(api_url, headers=headers)
-
+        contributors_response = requests.get(contributors_url)
         if contributors_response.status_code == 200:
-            contributors_data = json.loads(contributors_response.text)
+            contributors_data = contributors_response.json()
             contributors = len(contributors_data)
         else:
             contributors = "Error"
 
         # Get the latest release
-        releases_url = github_url + "/releases"
         releases_response = requests.get(releases_url)
         if releases_response.status_code == 200:
-            releases_soup = BeautifulSoup(releases_response.text, 'html.parser')
-            latest_release = releases_soup.select_one('.f1.flex-auto.min-width-0.text-normal a')
-            latest_release = latest_release.text.strip() if latest_release else "No release found"
+            releases_data = releases_response.json()
+            latest_release = releases_data[0]["tag_name"] if releases_data else "No release found"
         else:
             latest_release = "Error"
-
-        # Get the repository name and description
-        repo_name = github_url.split("/")[-1]
 
         # Find the existing markdown file with the repo name
         existing_files = [file for file in os.listdir() if re.search(rf"78\.\d{{2}}_{repo_name}\.md", file)]
@@ -72,29 +57,24 @@ for i, github_url in enumerate(urls, start=2):
             markdown_file = existing_files[0]
         else:
             markdown_file = f"78.{str(i).zfill(2)}_{repo_name}.md"
-        
-         
-            
+
         # Open the markdown file in append mode
         with open(markdown_file, "a") as f:
-            # Write the table header if the file doesn't exist
-            if not existing_files:
-                f.write(f'[repo_description](github_url)')
-                # Add the iframe with the README.md
-                readme_url = github_url.replace("github.com", "raw.githubusercontent.com") + "/main/README.md"
+            # Check if the file is empty
+            if os.stat(markdown_file).st_size == 0:
+                # Write the repo description and URL
+                f.write(f"## [{repo_description}]({github_url})\n")
                 f.write(f"\n## README\n")
                 f.write(f'<iframe width="100%" height="800" src="{readme_url}" ></iframe>\n')
                 
-                # Create a table
+                # Write the table header
                 f.write(f"\n##Overview\n")
                 f.write("| Date       | Watchers | Forks | Stars | Contributors | Latest Release |\n")
                 f.write("|------------|----------|-------|-------|--------------|----------------|\n")
 
             # Write the extracted data to the table
-            f.write(f"| {datetime.now().strftime('%Y-%m-%d')} | {watchers} | {forks} | {stars} | {contributors} | {latest_release} |\n")
+            f.write(f"| {datetime.now().strftime('%Y-%m-%d')} | {stars} | {watching} | {contributors} | {forks} | {latest_release} |\n")
 
-           
-            
         print(f"Data has been added to the markdown table for {repo_name}.")
     else:
         print("Error while fetching the GitHub page.")
